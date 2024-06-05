90173cafb36650a487fd14d09346be72e9da8d68
Nicolas Gracia <r0kk3nn@gmail.com>
2024-06-05 14:56:10 +0200

    liste

--- src/Controller/FrontCocktailController.php
+++ src/Controller/FrontCocktailController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Cocktail;
 use App\Form\CocktailType;
+use App\Repository\CocktailRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,17 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontCocktailController extends AbstractController
 {
     #[Route('/', name: 'front_cocktail_home')]
-    public function index(): Response
+    public function index(CocktailRepository $cocktailRepo): Response
     {
 
         $cocktail = new Cocktail();
-
         $formCocktail = $this->createForm(CocktailType::class, $cocktail);
+
+        $peoples = $cocktailRepo->findAll();
 
         // Rendre la vue avec le formulaire
         return $this->render('main/index.html.twig', [
             'controller_name' => 'Team builder',
-            'formCocktail' => $formCocktail->createView()
+            'formCocktail' => $formCocktail->createView(),
+            'peoples' => $peoples
         ]);
     }
 }
