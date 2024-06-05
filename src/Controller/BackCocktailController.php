f4f404c78f72605db4878a8fccf5c51fa699e6dc
Nicolas Gracia <r0kk3nn@gmail.com>
2024-06-05 15:29:43 +0200

    delete

--- src/Controller/BackCocktailController.php
+++ src/Controller/BackCocktailController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Cocktail;
 use App\Form\CocktailType;
+use App\Repository\CocktailRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class BackCocktailController extends AbstractController
 {
     #[Route('/ajouter', name: 'back_coktail_ajouter')]
-    public function index(EntityManagerInterface $em, Request $request): Response
+    public function ajouter(EntityManagerInterface $em, Request $request): Response
     {
 
         $cocktail = new Cocktail();
@@ -41,11 +42,51 @@ class BackCocktailController extends AbstractController
             $this->addFlash('primary', 'Nouveau : ' . $firstname . ' ' . $lastname);
             return $this->redirectToRoute("front_cocktail_home");
         }
-
-        // Rendre la vue avec le formulaire
-        // return $this->render('main/index.html.twig', [
-        //     'controller_name' => 'Team builder',
-        //     'formCocktail' => $formCocktail->createView()
-        // ]);
     }
+
+
+    #[Route('/update/{id}', name: 'back_coktail_update')]
+    public function update(EntityManagerInterface $em, Request $request): Response
+    {
+
+        // $cocktail = new Cocktail();
+
+        // $formCocktail = $this->createForm(CocktailType::class, $cocktail);
+
+        // $formCocktail->handleRequest($request);
+
+        // if ($formCocktail->isSubmitted() && $formCocktail->isValid()) {
+
+        //     $firstname = $formCocktail->get('firstname')->getData();
+        //     $lastname = $formCocktail->get('lastname')->getData();
+
+
+        //     $cocktail->setFirstname($firstname);
+        //     $cocktail->setLastname($lastname);
+
+
+        //     $em->persist($cocktail); 
+        //     $em->flush();
+
+        //     // Ajouter un message flash
+        //     $this->addFlash('primary', 'Nouveau : ' . $firstname . ' ' . $lastname);
+        //     return $this->redirectToRoute("front_cocktail_home");
+        // }
+    }
+
+    #[Route('/delete/{id}', name: 'back_coktail_delete')]
+    public function delete(Cocktail $cocktail,EntityManagerInterface $em): Response
+    {   
+        $firstname = $cocktail->getFirstname();
+        $lastname = $cocktail->getLastname();
+
+        $em->remove($cocktail);
+        $em->flush();  
+        $this->addFlash('danger', $firstname . ' ' . $lastname . ' supprimé avec succès.');
+
+        return $this->redirectToRoute("front_cocktail_home");
+        
+    }
+
+
 }
